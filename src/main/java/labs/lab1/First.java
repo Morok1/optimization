357ec976621ff8223a06d0091a10623e177daedd
zhenyasmagin@yandex.ru <zhenyasmagin@yandex.ru>
2019-07-27 23:30:21 +0300

    add performance tuning

--- src/main/java/labs/lab1/First.java
+++ src/main/java/labs/lab1/First.java
@@ -1,5 +1,11 @@
 package labs.lab1;
 
 public class First{
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 10000 ; i++) {
+            System.out.println();
+            Thread.sleep(100);
+        }
+    }
 
 }
