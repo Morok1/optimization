97d5e23f365bbc68fcb1e9a4b20aaa40a3d6bc4f
zhenyasmagin@yandex.ru <zhenyasmagin@yandex.ru>
2019-08-14 20:52:43 +0300

    small changes

--- src/main/java/labs/lab1/examples/par42objectreuse/ThreadLocals.java
+++ src/main/java/labs/lab1/examples/par42objectreuse/ThreadLocals.java
@@ -1,5 +1,32 @@
 package labs.lab1.examples.par42objectreuse;
 //The previous example of a pool manager can be used by
 // multiple threads in a multithreaded application,althoughthegetVector( )andreturnVector( )methodsfirstneedtobedefinedas synchronized .
+
+//references
+//http://samolisov.blogspot.com/2011/04/threadlocal.html
+/*
+Рассмотрим следующую задачу: нужно написать многопоточное приложение, в котором
+каждый поток участвует в построении некоторой структуры данных внутри некоторого класса-билдера,
+ при этом хочется отследить сколько записей в структуре построил тот или иной поток.*/
+
 public class ThreadLocals {
+    public static int[] array = new int[10];
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public class MyThread implements Runnable{
+        private int[] array;
+
+        public MyThread(int[] array) {
+            this.array = array;
+        }
+
+        @Override
+        public void run() {
+
+        }
+    }
 }
